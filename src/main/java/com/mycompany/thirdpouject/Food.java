2e6d40441b053ea114a4b5852592929e62726da0
katelynpou <kkvu58@gmail.com>
2026-04-17 19:54:50 -0700

    added two classes called food and games. might rename games to toys later depending on how well i can code a few minigames

---
+++ src/main/java/com/mycompany/thirdpouject/Food.java
@@ -0,0 +1,9 @@
+package com.mycompany.thirdpouject;
+
+public class Food {
+    private int hunger_value;
+
+    public Food(int hunger_val) {
+        this.hunger_value = hunger_val;
+    }
+}
---
+++ src/main/java/com/mycompany/thirdpouject/Games.java
@@ -0,0 +1,8 @@
+package com.mycompany.thirdpouject;
+
+public class Games {
+    private int entertainment_value;
+    public Games(int entertain) {
+        this.entertainment_value = entertain;
+    }
+}
